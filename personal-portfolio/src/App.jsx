7b976a296f36f4f7485b626054c99b64bd7b0f3a
abdulmalik360 <kaybee437@gmail.com>
2025-09-25 03:25:35 +0100

    third commit

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+import Navbar from "./Ui/Navbar";
+import MobileMenu from "./Ui/MobileMenu";
+
 function App() {
-  return <div className="text-3xl font-bold underline"> Home </div>
+ const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div className="text-3xl font-bold underline">
+<Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+<MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+</div>
+  );
 }
+
 export default App
